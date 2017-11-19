4297ac281794d4971fb7510bfda399253069e8bd
Xiao Wu <wuxiao199301@126.com>
2017-11-19 12:57:43 -0500

    Simulation Study

--- Bench_simulation.R
+++ Bench_simulation.R
@@ -10,9 +10,7 @@ setwd("~/Dropbox/PM2.5 Research/Simulation/More_simulation/Simulation_plot")
 
 subclass.MSE.bias<-mclapply(1:7,subclass.10level.fun,mc.cores=7)
 
-
 IPTW.MSE.bias<-mclapply(1:7,IPTW.trun1.fun,mc.cores=7)
-
 
 matching.MSE.bias<-mclapply(1:7,matching.fun,mc.cores=7)
 
