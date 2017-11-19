94be0cd622e9edd7782c739a050d97452a33d614
Xiao Wu <wuxiao199301@126.com>
2017-11-19 12:41:23 -0500

    Simulation study
    Conduct the simulation for the total of seven simulation settings. The code output Figure 1 in the main text and Figure A1-A8 in supplementary materials, as well as bias and mean square error for each setting.

---
+++ Bench_simulation.R
@@ -0,0 +1,19 @@
+library("parallel")
+
+source（"data.generate.R")
+source（"RC_IPTW_trun.R")
+source（"RC.adjust.10level.R")
+source（"RC_matching.R")
+
+setwd("~/Dropbox/PM2.5 Research/Simulation/More_simulation/Simulation_plot")
+
+
+subclass.MSE.bias<-mclapply(1:7,subclass.10level.fun,mc.cores=7)
+
+
+IPTW.MSE.bias<-mclapply(1:7,IPTW.trun1.fun,mc.cores=7)
+
+
+matching.MSE.bias<-mclapply(1:7,matching.fun,mc.cores=7)
+
+
